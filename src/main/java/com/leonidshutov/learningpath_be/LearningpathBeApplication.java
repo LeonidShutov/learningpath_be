21d5afa106ce3d24377fae13a938a46dc8de8ccc
Leonid Shutov <shizopotam@gmail.com>
2025-06-21 11:20:44 -0700

    init springboot commit

---
+++ src/main/java/com/leonidshutov/learningpath_be/LearningpathBeApplication.java
@@ -0,0 +1,13 @@
+package com.leonidshutov.learningpath_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningpathBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningpathBeApplication.class, args);
+	}
+
+}
